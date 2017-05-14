a9bc3d816188505e53931ff23feb243c2207f711
GabrielMioni <gabriel.mioni@gmail.com>
2017-05-14 11:28:35 -0700

    Added some documenation

--- rigby_root.php
+++ rigby_root.php
@@ -4,7 +4,8 @@
  * @package     Rigby
  * @author      Gabriel Mioni <gabriel@gabrielmioni.com>
  *
- * 
+ * The file defines RIGBY_ROOT which is used to provide an absolute file path for many files in the
+ * Rigby library.
  *
  */
 
