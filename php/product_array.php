7c41fba2b19a5bf223d3b59faa6baa7960c20172
GabrielMioni <gabriel.mioni@gmail.com>
2017-05-29 16:23:30 -0700

    Added live products to the  instead of hard coded testing array.

--- php/product_array.php
+++ php/product_array.php
@@ -1,14 +1,32 @@
 <?php
+if (!defined('RIGBY_ROOT'))
+{
+    require_once('../rigby_root.php');
+}
+require_once(RIGBY_ROOT . '/php/sql_pdo/sql_define.php');
+require_once(RIGBY_ROOT . '/php/sql_pdo/sql_pdo.php');
 
-$product_array = array(
-    'a' => 'Apples',
-    'b' => 'Blogna',
-    'c' => 'Clavendish',
-    'd' => 'Duke',
-    'e' => 'Espirit',
-    'f' => 'Franks',
-    'g' => 'Galapagos',
-    'h' => 'Hearst',
-    'i' => 'Indigo',
-    'j' => "Junebug"
-);
+class product_array
+{
+    static function get_product_array()
+    {
+        $tmp = array();
+        $query = "SELECT product_id, product_name FROM products";
+
+        try {
+            $results =  sql_pdo::run($query)->fetchAll();
+            foreach ($results as $product)
+            {
+                $product_id = $product['product_id'];
+                $product_name = $product['product_name'];
+                $tmp[$product_id] = $product_name;
+            }
+        } catch (PDOException $e)
+        {
+            error_log($e->getMessage());
+        }
+        return $tmp;
+    }
+}
+
+$product_array = product_array::get_product_array();
