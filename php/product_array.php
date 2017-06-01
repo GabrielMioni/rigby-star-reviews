41508aaa975459e66b9f4c3c9ede442fbff835d7
GabrielMioni <gabriel.mioni@gmail.com>
2017-05-31 21:05:21 -0700

    Product array is now created by calling a static method from products_array_set.php

--- php/product_array.php
+++ php/product_array.php
@@ -3,30 +3,6 @@ if (!defined('RIGBY_ROOT'))
 {
     require_once('../rigby_root.php');
 }
-require_once(RIGBY_ROOT . '/php/sql_pdo/sql_define.php');
-require_once(RIGBY_ROOT . '/php/sql_pdo/sql_pdo.php');
+require_once(RIGBY_ROOT . '/php/products_array_set.php');
 
-class product_array
-{
-    static function get_product_array()
-    {
-        $tmp = array();
-        $query = "SELECT product_id, product_name FROM products";
-
-        try {
-            $results =  sql_pdo::run($query)->fetchAll();
-            foreach ($results as $product)
-            {
-                $product_id = $product['product_id'];
-                $product_name = $product['product_name'];
-                $tmp[$product_id] = $product_name;
-            }
-        } catch (PDOException $e)
-        {
-            error_log($e->getMessage());
-        }
-        return $tmp;
-    }
-}
-
-$product_array = product_array::get_product_array();
+$product_array = product_array_set::get_product_array();
