7781ca3dcf84286513e1655bd24bbbc19b98ed46
GabrielMioni <gabriel.mioni@gmail.com>
2017-05-31 21:16:29 -0700

    Set index.php example page to use new widgets.

--- index.php
+++ index.php
@@ -2,16 +2,15 @@
 session_set_cookie_params(0);
 session_start();
 require_once('rigby_root.php');
-require_once('php/product_array.php');
+//require_once('php/product_array.php');
 require_once('php/build_review_form.php');
+require_once('widgets/submit_module.php');
+require_once('widgets/histogram.php');
+require_once('widgets/sidebar.php');
+require_once('php/paginator_reviews.php');
 
-/*
-$build_header = new page_header2('projects.php');
-$header = $build_header->return_header();
-*/
-
-$build_review_form = new build_review_form($product_array);
-$review_form = $build_review_form->return_review_form();
+$build_submit_module = new submit_module();
+$submit_module = $build_submit_module->return_submit_module();
 
 $current_year = date('Y', time());
 ?>
@@ -30,41 +29,30 @@ $current_year = date('Y', time());
     <body>
         <div id='content'>
             <div id='left'>
-                <?php echo $review_form; ?>
+                <?php
+                echo $submit_module;
+                ?>
             </div>
             <div id='right'>
                 <?php
-                require_once('php/review_widget.php');
 
-                $build_widget = new review_widget('');
-                echo $build_widget->return_widget();
-                ?>
-                <div id='review_col'>
-                    <?php
-                    require_once('php/sidebar.php');
-                    require_once('php/paginator_reviews.php');
+                $build_histogram = new histogram('');
+                echo $build_histogram->return_histogram();
 
-                    /* Configuration variables for sidebar and paginator classes */
-                    $page = '';
-                    $rating = '';
-                    $reviews_per_page = '';
-                    $buttons_per_page = 10;
-                    $product = '';
+                /* Configuration variables for sidebar and paginator classes */
+                $page = '';
+                $reviews_per_page = '';
+                $rating = '';
+                $buttons_per_page = 10;
+                $product_id = '';
 
-                    $build_sidebar = new sidebar($page, $rating, $reviews_per_page, $product, $product_array);
-                    $sidebar = $build_sidebar->getSidebarHtml();
-                    echo $sidebar;
-                    ?>
-                </div>
-                <?php
+                $build_sidebar = new sidebar2($page, $reviews_per_page, $rating, $product_id);
+                $sidebar = $build_sidebar->return_sidebar();
+                echo $sidebar;
+
                 $build_pagination = new paginator_reviews($page, $reviews_per_page, $buttons_per_page, $rating);
                 $bar = $build_pagination->get_pagination_bar();
                 echo $bar;
-                /* Experiment
-                    $build_pagination = new paginator($page, $rating, $reviews_per_page, $buttons_per_page);
-                    $bar = $build_pagination->get_pagination_bar();
-                    echo $bar;
-                */
                 ?>
             </div>
         </div>
@@ -74,9 +62,9 @@ $current_year = date('Y', time());
             </div>
         </div>
         <script type='text/javascript' src='../../scripts/js/main.js'></script>
-        <script type='text/javascript' src='js/review_stars.js'></script>
-        <script type='text/javascript' src='js/review_navigation.js'></script>
-        <script type='text/javascript' src='js/js_fom.js'></script>
+        <!--<script type='text/javascript' src='js/review_stars.js'></script>-->
+        <!--<script type='text/javascript' src='js/review_navigation.js'></script>-->
+        <!--<script type='text/javascript' src='js/js_fom.js'></script>-->
     </body>
     
 </html>
