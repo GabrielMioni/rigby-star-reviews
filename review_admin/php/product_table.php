f02e8171c325992ac481968c4c1a0389c74bd0b0
GabrielMioni <gabriel.mioni@gmail.com>
2017-05-31 21:06:26 -0700

    Added query to get 'Review Counts' column.

--- review_admin/php/product_table.php
+++ review_admin/php/product_table.php
@@ -26,7 +26,8 @@ class product_table {
         $this->page         = $this->check_input($page);
         $this->results_per_page = $this->check_input($results_per_page);
 
-        $this->results = $this->set_results($this->product_name, $this->product_id, $this->date_set, $this->date_start, $this->date_end, $this->page, $this->results_per_page);
+//        $this->results = $this->set_results($this->product_name, $this->product_id, $this->date_set, $this->date_start, $this->date_end, $this->page, $this->results_per_page);
+        $this->results = $this->set_results($this->product_name, $this->product_id, $this->page, $this->results_per_page);
 
         $this->table = $this->build_table($this->results);
 
@@ -42,15 +43,48 @@ class product_table {
         }
     }
 
-    protected function set_results($product_name, $product_id, $date_set, $date_start, $date_end, $page, $results_per_page)
+//    protected function set_results($product_name, $product_id, $date_set, $date_start, $date_end, $page, $results_per_page)
+    protected function set_results($product_name, $product_id, $page, $results_per_page)
     {
+        $pdo = array();
+        
         $row_start = $this->set_start($page, $results_per_page);
         $row_end   = $row_start + $results_per_page;
+        
+        $query = '  SELECT  p.id, p.product_name, p.product_id, p.create_date, p.last_review, COUNT(r.product) as review_count
+                    FROM    products p
+                    LEFT JOIN
+                            star_reviews r
+                    ON      r.product = p.product_id';
+        if ($product_name !== '')
+        {
+            $query .= ' WHERE p.product_name = ? AND';
+            $pdo[] = $product_name;
+        }
+        if ($product_id !== '')
+        {
+            $query .= ' WHERE p.product_id = ?';
+            $pdo[] = $product_id;
+        }
+        $query = rtrim($query, ' AND');
+        $query .= " GROUP BY p.product_id ORDER BY p.id LIMIT ?, ?";
+        $pdo[] = $row_start;
+        $pdo[] = $results_per_page;
 
-
-        $get_product_rows = new products_search($product_name, $product_id, $date_set, $date_start, $date_end, $row_start, $row_end, true);
-        return $get_product_rows->return_results();
+        try {
+            $results = sql_pdo::run($query, $pdo)->fetchAll();
+            return $results;
+        } catch (PDOException $e) {
+            error_log($e->getMessage());
+            return array();
+        }
     }
+    
+    protected function push_pdo($value)
+    {
+        
+    }
+        
 
     protected function set_start($page, $results_per_page)
     {
