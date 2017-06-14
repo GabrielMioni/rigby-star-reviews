f38715fafd0f5afa9afc64a72174e65cfffd926a
GabrielMioni <gabriel.mioni@gmail.com>
2017-06-13 20:03:44 -0700

    Changed logout.php to use JSON encoded  to display messages

--- review_admin/logout.php
+++ review_admin/logout.php
@@ -25,9 +25,29 @@ class logout {
     }
     public function redirect() {
         session_start();
-        $_SESSION['logout_msg'] = 1;
+//        $_SESSION['logout_msg'] = 1;
+        $this->set_message(true, 'You have been logged out');
         $login_page = $this->login_page;
         header('Location: '.$login_page);        
+    }
+
+    /**
+     * Builds an array with data that's used to set a response message that displays for the user on forgot_password.php
+     * The array is JSON encoded and set as a element at $_SESSION['pswd_reset_result].
+     *
+     * @param $status bool  Sets the first element in the array. False = 0, True = 1. 0 is for displaying 'error' messages,
+     *                      1 is for 'good' messages.
+     * @param $msg string   The message that should be displayed to the user.
+     */
+    protected function set_message($status, $msg)
+    {
+        $json_array = array();
+        $json_array[] = $status == true ? 1 : 0;
+        $json_array[] = $msg;
+
+        $json_string = json_encode($json_array);
+
+        $_SESSION['pswd_reset_result'] = $json_string;
     }
 }
 
